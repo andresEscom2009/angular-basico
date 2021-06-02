114537e92e017dadaf5013d7993b6be95cf47732
Andres Leon <andres2009escom@gmail.com>
2021-06-02 14:09:42 -0500

    primer commit

---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+  styleUrls: ['./listado.component.css']
+})
+
+
+export class ListadoComponent {
+
+  heroes: string[] = ['Spiderman', 'Ironman','Hulk', 'Thor'];
+  heroeBorrado: string = '';
+
+
+  borrarHeroe(): void{
+    this.heroeBorrado =  this.heroes.pop() || '';
+  
+  }
+
+  isEmptyHeroe(): boolean{
+    return this.heroeBorrado.length != 0 ;
+  }
+
+
+}
